d8ba86a9c7c730c690313a09605e1b0688840089
codewitch-honey-crisis <gaztoast@gmail.com>
2021-05-29 09:34:41 -0700

    fixed assembly name/properties/namespace

--- AutoUpdateTest/Properties/AssemblyInfo.cs
+++ AutoUpdateTest/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AutoUpdate")]
+[assembly: AssemblyTitle("AutoUpdateTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AutoUpdate")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("AutoUpdateTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("62e2cb7a-ad1b-450b-840d-f7138c5af9f5")]
+[assembly: Guid("F11D2BB1-DAE4-4E17-A394-7D47326E8090")]
 
 // Version information for an assembly consists of the following four values:
 //
